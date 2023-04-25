ecf712fd7e5b4a22826cbd46cffd3aff40d3045e
ChilledAatma <jayantijerai@gmail.com>
2023-04-25 18:13:18 +0530

    HomePage slider change

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,37 @@ window.addEventListener("scroll", function () {
 backToTopBtn.addEventListener("click", function () {
     window.scrollTo(0, 0);
 });
+// $(document).ready(function () {
+//   function initCarousel() {
+//     if ($("#visible").css("display") == "block") {
+//       $(".carousel .carousel-item").each(function () {
+//         var i = $(this).next();
+//         i.length || (i = $(this).siblings(":first")),
+//           i.children(":first-child").clone().appendTo($(this));
+
+//         for (var n = 0; n < 4; n++)
+//           (i = i.next()).length || (i = $(this).siblings(":first")),
+//             i.children(":first-child").clone().appendTo($(this));
+//       });
+//     }
+//   }
+//   $(window).on({
+//     resize: initCarousel(),
+//     load: initCarousel()
+//   });
+// });
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 2
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
